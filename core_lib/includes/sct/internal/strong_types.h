25d14d4ef1359d2c62f6e917d8e2ae07a6bddd36
rpeschke <richard.peschke@desy.de>
2016-04-17 13:00:32 +0200

    added strong_types to project

---
+++ core_lib/includes/sct/internal/strong_types.h
@@ -0,0 +1,62 @@
+#ifndef strong_types_h__
+#define strong_types_h__
+
+
+#include <string>
+#include "sct/platform.hh"
+#ifdef _DEBUG
+
+
+#define necessary_CONVERSION(x) x.value
+#define Un_necessary_CONVERSION(x) x.value
+
+#define TYPE_CLASS(name,type) \
+class DllExport name { \
+public: \
+  explicit name(const type& param_) :value(param_) {}\
+  type value; \
+}
+#define TYPE_CLASS_PTR(name,type) \
+class DllExport name { \
+public: \
+  explicit name(type param_) :value(param_) {}\
+  type value; \
+}
+
+
+#else 
+#define necessary_CONVERSION(x) x
+#define Un_necessary_CONVERSION(x) x
+
+#define TYPE_CLASS(name,type) \
+typedef  type name
+#define TYPE_CLASS_PTR(name,type) \
+typedef  type name
+
+#endif // _DEBUG
+
+template <typename out_t, typename in_t>
+out_t strong_cast(const in_t& in_) {
+  return out_t(necessary_CONVERSION(in_));
+}
+
+
+  TYPE_CLASS(modulo_x, double);
+  TYPE_CLASS(modulo_y, double);
+  TYPE_CLASS(modulo_t, double);
+
+
+  TYPE_CLASS(ID_t, double);
+  TYPE_CLASS(stripNr_t, int);
+  TYPE_CLASS(residualCut_t, double);
+  TYPE_CLASS(rot_angle_t, double);
+
+  TYPE_CLASS(move_t, double);
+  TYPE_CLASS(collectionName_t, std::string);
+  TYPE_CLASS(AxesName_t, std::string);
+  TYPE_CLASS(procent_t, double);
+
+
+
+
+#endif // strong_types_h__
